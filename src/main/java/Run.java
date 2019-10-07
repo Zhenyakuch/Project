91622d48b8677404729d55b3afe45a73f3ce911c
Zhenyakuch <zhenya_247@mail.ru>
2019-10-07 08:02:46 +0300

    commit Translator

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -7,10 +7,9 @@ public class Run {
         do {
             System.out.print("Введите целое число в промежутке  от -999 999 999 999 до 999 999 999 999, для выхода введите 0: ");
             number = scanner.nextInt();
-            System.out.println(Translator.translate_rus(number));
+            System.out.println(Translator.translateToString(number));
         } while (number != 0);
         System.out.println("Введено число /Ноль/ Работа программы завершена");
     }
 
     }
-}
